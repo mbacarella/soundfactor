6382503f59b3f6332f941c874c4a370501308c11
Michael Bacarella <mbac@panix.com>
2014-10-13 21:38:13 +0100

    re-org some modules, mid-way through

--- src/soundfactor/command.clj
+++ src/soundfactor/command.clj
@@ -6,48 +6,54 @@
 
 (defn print-usage [command-or-group breadcrumbs]
   ; TODO: stderr
-  (let [tag (first command-or-group)]
+  (let [tag        (first command-or-group)
+        cmd-as-map (apply hash-map (second command-or-group))
+        crumbs     (clojure.string/join " " (reverse breadcrumbs))]
     (printf "%s\n" (get-summary command-or-group))
     (printf "\n")
     (cond (= tag :command)
-            (let [cmd-as-map (apply hash-map (second command-or-group))]
-              (printf "  argv[0] %s <args>\n" (reverse breadcrumbs))
-              (printf "\n")
-              (printf "___ flags ___\n")
-              (doseq [spec (cmd-as-map :spec-list)]
-                (printf "  spec: %s\n" spec))
-              (flush))
+          (let [spec-list  (cmd-as-map :spec)]
+            (printf "  argv[0] %s<args>\n" crumbs)
+            (printf "\n")
+            (if (not (empty? spec-list))
+              (do (printf "___ flags ___\n\n")
+                  (doseq [spec spec-list]
+                    (printf "  spec: %s\n" spec))))
+            (printf "\n")
+            (flush))
           (= tag :group) 
-            (let [cmd-as-map (apply hash-map (second command-or-group))]
-              (printf "  argv[0] %s\n" (reverse breadcrumbs))
-              (printf "\n")
-              (printf "___ subcommands ___\n\n")
-              (doseq [[name sub-command] (cmd-as-map :subcommands)]
-                (printf "  %s          %s\n" name (get-summary sub-command)))
-              (printf "\n")
-              (flush))
-            :else (assert false "print-usage: did not get :command or :group"))))
+          (let [subcommands (cmd-as-map :subcommands)]
+            (printf "  argv[0] %s\n" crumbs)
+            (printf "\n")
+            (if (not (empty? subcommands))
+              (do (printf "___ subcommands ___\n\n")
+                  (doseq [[name sub-command] (cmd-as-map :subcommands)]
+                    (printf "  %s          %s\n" name (get-summary sub-command)))))
+            (printf "\n")
+            (flush))
+          :else (assert false "print-usage: did not get :command or :group"))))
 
 (defn print-usage-error [command-or-group breadcrumbs s] 
   (print-usage command-or-group breadcrumbs)
-  (printf "error: %s\n" s) ; TODO: stderr
+  (println s) ; TODO: stderr
   (flush)
   (System/exit 1))
 
 (defn process-flag [spec-as-map args-for-main argv]
-  (let [switch           (spec-as-map :flag)
-        partial-matches  (filter (fn [arg] (.startsWith switch arg)) argv)]
+  (let [starts-with      (fn [haystack needle] (if (nil? needle) false (.startsWith haystack needle)))
+        switch           (spec-as-map :flag)
+        partial-matches  (filter (fn [arg] (starts-with switch arg)) argv)]
     (if (> (count partial-matches) 1)
       ;; we allow the user to specify less than the full name of the flag so long as it uniquely
       ;; identifies the flag, so let's just make sure each flag matches once at most
       ;; it's true, this seems N^2, but if your program accepts 10,000 flag arguments you probably suck
       [:usage-error (format "multiple matches for flag '%s': %s" switch (apply str partial-matches))]
-      (let [argv-before           (take-while (fn [arg] (not (.startsWith switch arg))) argv)
+      (let [argv-before           (take-while (fn [arg] (not (starts-with switch arg))) argv)
             argv-middle-and-after (drop (count argv-before) argv)
             type-info             (spec-as-map :type)
             type-tag              (first type-info)
             return-ok             (fn [x] [:ok x])]
-        (if (.startsWith switch (first argv-middle-and-after)) ; was switch found?
+        (if (starts-with switch (first argv-middle-and-after)) ; was switch found?
           (let [[value argv-after] ; yes! process it
                 (if (= type-tag :no-arg) 
                   [true (rest argv-middle-and-after)]
@@ -56,7 +62,7 @@
             (return-ok [(concat args-for-main [value]) 
                         (concat argv-before argv-after)]))
           ;; switch not found. maybe it's optional?
-          (let [return-value (fn [value] (return-ok [(concat args-for-main [value])] argv))]
+          (let [return-value (fn [value] (return-ok [(concat args-for-main [value]) argv]))]
             (match [type-tag]
                    [:required] [:usage-error (format "the '%s' flag is required" switch)]
                    [:optional] (return-value nil)
@@ -83,7 +89,12 @@
                   [[] argv]
                   spec-list)]
       (if (not (empty? argv-leftovers))
-        (print-usage-error cmd-wrap breadcrumbs (format "too many anonymous arguments: %s" argv))
+        (print-usage-error cmd-wrap 
+                           breadcrumbs 
+                           (let [extra (first argv-leftovers)]
+                             (if (.startsWith extra "-")
+                               (format "unknown flag: %s" extra)
+                               (format "too many anonymous arguments: %s" extra))))
         (apply main args-for-main)))))
 
 (defn process-command-or-group [command-or-group argv breadcrumbs]
