b720e5ad34d03a64ddd41cfe1519f1121fb951b7
Michael Bacarella <mbac@panix.com>
2015-02-18 18:56:08 +0000

    better

--- src/soundfactor/beat-this.clj
+++ src/soundfactor/beat-this.clj
@@ -5,24 +5,47 @@
   (:import [soundfactor.util :as util])
 )
 
-(def band-limits (apply vector [0 200 400 800 1600 3200]))
-(def nbands      (count band-limits))
-(def max-freq    4096)
-(def floor       Math/floor)
+(defn array-init [len f]
+  (float-array len (map f (range len))))
 
-(defn filter-bank [time-series]
-  (let [data   (double-array time-series)
-        n      (count data)
-        dft    (util/fft data)  
+(defrecord CrappyMatrix [rows columns array])
+
+(defn matrix-new [rows columns]
+  (CrappyMatrix. rows columns (float-array (* rows columns) 0)))
+
+;; XXX: test me
+(defn matrix-splice [matrix first-row last-row col data]
+  (doseq [i (range (- last-row first-row))]
+    (let [index (+ col (* (+ first-row i) (:columns matrix)))]
+      (aset (:array matrix) index (aget data i)))))
+   
+(defn slice [array start end]
+  (subvec array start end))
+
+(def bandlimits 
+  (let [bandlimits [0 200 400 800 1600 3200]]
+    (int-array (count bandlimits) bandlimits)))
+(def maxfreq 4096)
+(def floor Math/floor)
+(def nbands (count bandlimits))
+
+(defn filter-bank [sig]
+  (let [dft        (fft sig)
+        n          (count dft)
+        nbands     (count bandlimits)
         ;; bring band scale from Hz to the points in our vectors
-        bl     (conj (map (fn [i] (inc (floor (/ (get band-limits i) (* max-freq n) 2))))
-                          (range (dec n)))
-                     (floor (inc (/ (get band-limits (dec nbands)) (* max-freq n) 2))))
-        br     (conj (map (fn [i]  (floor (/ (get band-limits (inc i)) (* max-freq n) 2)))
-                          (range (dec n)))
-                     (floor (/ n 2)))
-        output (zeroes n nbands)
-    ;; create the frequency bands and put them in the vector output
-    
-
-)))
+        bl         (array-init nbands (fn [i] (inc (floor (/ (* (/ (aget bandlimits i) maxfreq) n))))))
+        br         (array-init nbands (fn [i] (if (= (dec nbands)) (floor (/ n 2))
+                                                  (floor (/ (* (/ (aget bandlimits (inc i)) maxfreq) n) 2)]))
+        output     (matrix-new n nbands))]
+    (doseq [i (range nbands)]
+      (let [bl_i (aget bl i)
+            br_i (aget br i)
+            ;; XXX: i think the +1 isn't needed?
+            np1  (inc n)
+            np1_m_br_i (- np1 br_i)
+            np1_m_bl_i (- np1 bl_i)]
+        (matrix-splice output bl_i br_i i (slice dft bl_i br_i))
+        (matrix-splice output np1_m_br_i np1_m_bl_i i (slice dft np1_br_i np1_m_bl_i))))
+    (aset (:array matrix) 0 0)
+    matrix))
